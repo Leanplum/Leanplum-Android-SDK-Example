df7ddb842517add8cc3f35b3998d7119e03a6b46
Ben Marten <ben.marten@me.com>
2017-07-16 11:22:05 -0400

    refactor(testing fragment): refactor fragment

--- app/src/main/java/com/leanplum/android/example/fragments/TestingFragment.java
+++ app/src/main/java/com/leanplum/android/example/fragments/TestingFragment.java
@@ -1,7 +1,7 @@
 package com.leanplum.android.example.fragments;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,50 +10,38 @@ import android.widget.Button;
 import com.leanplum.Leanplum;
 import com.leanplum.android.example.R;
 
-/**
- * Created by sayaan on 7/10/17.
- */
-
 public class TestingFragment extends Fragment {
-    public TestingFragment() {
-    }
+  public TestingFragment() {
+  }
 
-    public static TestingFragment newInstance() {
-        return new TestingFragment();
-    }
+  public static TestingFragment newInstance() {
+    return new TestingFragment();
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                         Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.testing_layout, container, false);
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View root = inflater.inflate(R.layout.testing_layout, container, false);
 
-        Button example = (Button) root.findViewById(R.id.track_event);
-        example.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Leanplum.track("Example");
-            }
-        });
+    Button loadTest = (Button) root.findViewById(R.id.load_test);
+    loadTest.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        for (int i = 0; i < 10002; i++) {
+          Leanplum.track("load_test_iteration" + i);
+        }
+      }
+    });
 
-        Button loadTest = (Button) root.findViewById(R.id.load_test);
-        loadTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                for (int i = 0; i < 10002; i++) {
-                    Leanplum.track("load_test_iteration" + i);
-                }
-            }
-        });
+    Button crashApp = (Button) root.findViewById(R.id.crash_app);
+    crashApp.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        throw new RuntimeException("This is a crash");
+      }
 
-        Button crashApp = (Button) root.findViewById(R.id.crash_app);
-        crashApp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                throw new RuntimeException("This is a crash");
-            }
+    });
 
-        });
-
-        return root;
-    }
+    return root;
+  }
 }
